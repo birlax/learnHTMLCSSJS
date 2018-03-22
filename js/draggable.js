c023e726c4fb01fe211ef2833b65afdd3e4213a6
Birlax <birlax@gmail.com>
2018-03-23 00:55:17 +0530

    Adding few more feature presentations to grid Reviewer:Self

--- js/draggable.js
+++ js/draggable.js
@@ -1,52 +1,67 @@
 var dragged;
 
-  /* events fired on the draggable target */
-  document.addEventListener("drag", function( event ) {
+/* events fired on the draggable target */
+document.addEventListener("drag", function (event) {
 
-  }, false);
+}, false);
 
-  document.addEventListener("dragstart", function( event ) {
-      // store a ref. on the dragged elem
-      dragged = event.target;
-      // make it half transparent
-      event.target.style.opacity = .5;
-  }, false);
+document.addEventListener("dragstart", function (event) {
+    // store a ref. on the dragged elem
+    dragged = event.target;
+    // make it half transparent
+    event.target.style.opacity = .5;
+}, false);
 
-  document.addEventListener("dragend", function( event ) {
-      // reset the transparency
-      event.target.style.opacity = "";
-  }, false);
+document.addEventListener("dragend", function (event) {
+    // reset the transparency
+    event.target.style.opacity = "";
+}, false);
 
-  /* events fired on the drop targets */
-  document.addEventListener("dragover", function( event ) {
-      // prevent default to allow drop
-      event.preventDefault();
-  }, false);
+/* events fired on the drop targets */
+document.addEventListener("dragover", function (event) {
+    // prevent default to allow drop
+    event.preventDefault();
+}, false);
 
-  document.addEventListener("dragenter", function( event ) {
-      // highlight potential drop target when the draggable element enters it
-      if ( event.target.className == "dropzone" ) {
-          event.target.style.background = "purple";
-      }
+document.addEventListener("dragenter", function (event) {
+    // highlight potential drop target when the draggable element enters it
+    if (event.target.className == "dropzone") {
+        event.target.style.background = "purple";
+    }
 
-  }, false);
+}, false);
 
-  document.addEventListener("dragleave", function( event ) {
-      // reset background of potential drop target when the draggable element leaves it
-      if ( event.target.className == "dropzone" ) {
-          event.target.style.background = "";
-      }
+document.addEventListener("dragleave", function (event) {
+    // reset background of potential drop target when the draggable element leaves it
+    if (event.target.className == "dropzone") {
+        event.target.style.background = "";
+    }
 
-  }, false);
+}, false);
 
-  document.addEventListener("drop", function( event ) {
-      // prevent default action (open as link for some elements)
-      event.preventDefault();
-      // move dragged elem to the selected drop target
-      if ( event.target.className == "dropzone" ) {
-          event.target.style.background = "";
-          dragged.parentNode.removeChild( dragged );
-          event.target.appendChild( dragged );
-      }
-    
-  }, false);
+document.addEventListener("drop", function (event) {
+    // prevent default action (open as link for some elements)
+    event.preventDefault();
+    // move dragged elem to the selected drop target
+    if (event.target.className == "dropzone") {
+        event.target.style.background = "";
+        dragged.parentNode.removeChild(dragged);
+        event.target.appendChild(dragged);
+    }
+
+}, false);
+
+
+let terminalText = "Grid SQL > ";
+document.getElementById("terminal-1")
+    .addEventListener("keyup", function (event) {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            // document.getElementById("terminal-1-submit").click();
+            var txt = document.getElementById("terminal-1").value;
+            txt = txt.trim();
+            txt = txt.replace(terminalText,"");
+            document.getElementById("terminal-1").value=terminalText;
+            alert(txt);
+        }
+    });
